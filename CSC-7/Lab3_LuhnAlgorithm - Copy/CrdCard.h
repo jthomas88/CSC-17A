4f10609d302012fdd4d99a334cf5a3c3d1d3ecce
rcc <thomasjeffrey96@hotmail.com>
2017-03-20 15:05:22 -0700

    Signed-off-by: rcc <thomasjeffrey96@hotmail.com>

---
+++ CSC-7/Lab3_LuhnAlgorithm - Copy/CrdCard.h
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   CrdCard.h
+ * Author: Jeff
+ *
+ * Created on March 14, 2017, 9:09 PM
+ */
+
+#ifndef CRDCARD_H
+#define CRDCARD_H
+
+struct CrdCard{
+    int  length;
+    char startDg;
+    
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CRDCARD_H */
+
---
+++ CSC-7/Lab3_LuhnAlgorithm/build/Debug/Cygwin-Windows/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin-Windows/main.o: main.cpp CrdCard.h
+
+CrdCard.h:
